10780a8646e6716b89f7bcc37c53cdccaf61095e
callum <c.rollo@outlook.com>
2019-10-30 13:53:48 +0000

    Initialised cookie cutter repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Tools for setting up, running and processing data from a Seaglider fitted with a Nortek 1MHz ',
+    author='Callum Rollo',
+    license='BSD-3',
+)
