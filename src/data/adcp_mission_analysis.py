633096e8306e738849e6d1f24c1c7b46d28362cf
Callum Rollo <c.rollo@outlook.com>
2020-02-22 17:17:23 -0400

    Adding percent good corr to adcp dataframe

--- src/data/adcp_mission_analysis.py
+++ src/data/adcp_mission_analysis.py
@@ -18,8 +18,10 @@ except NameError:
     library_dir = Path('/media/callum/storage/Documents/adcp-glider/')
 sys.path.append(str(library_dir))
 from src.data.beam_mapping import beam2enu, beam_from_center, beam2xyz
+
 # todo hard coded for now. Will fix
 mission_lat = 13
+
 
 def list_yos(working_dir):
     yos_path = working_dir.rglob("*cp*.nc")
@@ -48,10 +50,14 @@ def direction_num_to_climb_phase(num_in):
         return 'Ascent'
     return 'Horizontal'
 
+
 def rounder(float_in, n=2):
     # To neaten cell sizes and distances for plots
     return round(float_in, n)
+
+
 rounder = np.vectorize(rounder)
+
 
 @dataclass
 class adcp_profile:
@@ -76,6 +82,7 @@ class adcp_profile:
     glider_w_from_p: float
     ad2cp_dict: dict
 
+
 def read_glider_nc(glider_dir):
     yos_path = glider_dir.rglob("p*.nc")
     yos_list = []
@@ -87,17 +94,19 @@ def read_glider_nc(glider_dir):
     for net in nets:
         nc = xr.open_dataset(net)
         dive_num = net[-7:-3]
-        dive_nums = dive_nums + list(np.tile(dive_num,(1,len(nc.pressure)))[0])
+        dive_nums = dive_nums + list(np.tile(dive_num, (1, len(nc.pressure)))[0])
         time = time + list(nc.time.data)
         pressure = pressure + list(nc.pressure.data)
         roll = roll + list(nc.eng_rollAng.data)
-        pitch = pitch +list(nc.eng_pitchAng.data)
+        pitch = pitch + list(nc.eng_pitchAng.data)
         heading = heading + list(nc.eng_head.data)
         temp = nc.temperature.data
         conductivity = nc.conductivity.data
         lon = nc.longitude
         lat = nc.latitude
-    glider_df = pd.DataFrame({'dive_num': dive_nums, 'pressure_gl': pressure, 'pitch_gl': pitch, 'roll_gl': roll, 'heading_gl': heading},index=pd.to_datetime(time))
+    glider_df = pd.DataFrame(
+        {'dive_num': dive_nums, 'pressure_gl': pressure, 'pitch_gl': pitch, 'roll_gl': roll, 'heading_gl': heading},
+        index=pd.to_datetime(time))
     return glider_df
 
 
@@ -153,8 +162,8 @@ def adcp_import_data(working_dir):
             time[i] = datetime(1970, 1, 1, 00, 00) + timedelta(
                 seconds=time_secondsfrom1970[i]
             )
-        cell_center = adcp_dict['blank_dist'] + np.arange(adcp_dict['cell_size']/2, adcp_dict['cell_size']*15,
-                                                           adcp_dict['cell_size'])
+        cell_center = adcp_dict['blank_dist'] + np.arange(adcp_dict['cell_size'] / 2, adcp_dict['cell_size'] * 15,
+                                                          adcp_dict['cell_size'])
         cell_center_neat = rounder(cell_center)
 
         # extract velocity, correlation and amplitude data from the beams
@@ -173,7 +182,7 @@ def adcp_import_data(working_dir):
         pressure = data_av["Pressure"][:]
         # glider_z = 0.7 + gsw.z_from_p(pressure, 50)
         # todo # Sort out gsw and document how to get it
-        glider_z =  gsw.z_from_p(pressure,mission_lat)
+        glider_z = gsw.z_from_p(pressure, mission_lat)
         dz = glider_z[1:] - glider_z[:-1]
         dt_datetime = time[1:] - time[:-1]
         dt_sec = []
@@ -201,13 +210,14 @@ def adcp_import_data(working_dir):
             for cell in np.arange(np.size(vel_beam, 1)):
                 vel_enu[sample, cell, :] = beam2enu(vel_beam[sample, cell, :], pitch[sample], roll[sample],
                                                     heading[sample], dive_limb=adcp_dict['vert_direction'])
-        beam_miss = beam_from_center(np.transpose(np.tile(pitch,(len(cell_center),1))),
-                                     np.transpose(np.tile(roll,(len(cell_center),1))),
-                                     np.tile(cell_center, (len(pitch),1)))
+        beam_miss = beam_from_center(np.transpose(np.tile(pitch, (len(cell_center), 1))),
+                                     np.transpose(np.tile(roll, (len(cell_center), 1))),
+                                     np.tile(cell_center, (len(pitch), 1)))
         ad2cp_dict = data_av.variables
 
         profile = adcp_profile(str(index), time, cell_center, pitch, roll, heading, cor_beam, amp_beam, vel_beam,
-                               vel_xyz, vel_enu, beam_miss, beam_number, pressure, glider_z, glider_w_from_p, ad2cp_dict)
+                               vel_xyz, vel_enu, beam_miss, beam_number, pressure, glider_z, glider_w_from_p,
+                               ad2cp_dict)
         profiles_dict[index] = profile
     # Add the per profile info to the mission summary
     for extra in extras_list:
@@ -220,6 +230,7 @@ def adcp_import_data(working_dir):
     # Everything else will go in a dictionary
     return mission_summary, profiles_dict
 
+
 def add_dive_averages(mission_summary, profiles_dict, combine=False):
     """
 
@@ -229,10 +240,11 @@ def add_dive_averages(mission_summary, profiles_dict, combine=False):
     :return: Data frame with information averaged over cell 5, 11 m from the glider, dataframe of attitude over time
     """
     beam_attrs = pd.DataFrame(index=mission_summary.index, columns=['cor_beam_1', 'cor_beam_2', 'cor_beam_3',
-                                                             'cor_beam_4', 'amp_beam_1', 'amp_beam_2',
-                                                             'amp_beam_3', 'amp_beam_4', 'beam_miss',
-                                                            'pitch', 'roll', 'heading',
-                                                                   'good_angle'])
+                                                                    'cor_beam_4', 'amp_beam_1', 'amp_beam_2',
+                                                                    'amp_beam_3', 'amp_beam_4', 'beam_miss',
+                                                                    'pitch', 'roll', 'heading',
+                                                                    'good_angle_5', 'good_angle_all', 'good_cor_5',
+                                                                    'good_cor_all'])
 
     cast_num, pressure, time, roll, pitch, heading = [], [], [], [], [], []
     for cycle in mission_summary.index:
@@ -254,21 +266,28 @@ def add_dive_averages(mission_summary, profiles_dict, combine=False):
             beam_attrs.amp_beam_1[cycle] = np.nanmean(amps[:, 5, 0])
             beam_attrs.amp_beam_2[cycle] = np.nanmean(amps[:, 5, 1])
             beam_attrs.amp_beam_4[cycle] = np.nanmean(amps[:, 5, 2])
-        beam_attrs.beam_miss[cycle] = np.nanmean(profiles_dict[cycle].beam_miss[:,5])
+        beam_attrs.beam_miss[cycle] = np.nanmean(profiles_dict[cycle].beam_miss[:, 5])
         beam_attrs.pitch[cycle] = np.nanmean(profiles_dict[cycle].pitch)
         beam_attrs.roll[cycle] = np.nanmean(np.abs(profiles_dict[cycle].roll))
         beam_attrs.heading[cycle] = np.nanmean(profiles_dict[cycle].heading)
-        beam_attrs.good_angle[cycle] = 100*sum(profiles_dict[cycle].beam_miss[:,5]<1.0)/len(profiles_dict[cycle].beam_miss)
+        beam_attrs.good_angle_5[cycle] = 100 * sum(profiles_dict[cycle].beam_miss[:, 5] < 1.0) / len(
+            profiles_dict[cycle].beam_miss)
+        beam_attrs.good_angle_all[cycle] = 100 * sum(profiles_dict[cycle].beam_miss[:, -1] < 1.0) / len(
+            profiles_dict[cycle].beam_miss)
+        beam_attrs.good_cor_5[cycle] = 100 * np.nansum(cors[:, 5:, :] > 50) / np.size(cors[:, :5, :])
+        beam_attrs.good_cor_all[cycle] = 100 * np.nansum(cors[:, :, :] > 50) / np.size(cors[:, :, :])
         time = time + list(profiles_dict[cycle].time)
-        cast_num = cast_num + list(np.tile(profiles_dict[cycle].name, (1,len(profiles_dict[cycle].time)))[0])
+        cast_num = cast_num + list(np.tile(profiles_dict[cycle].name, (1, len(profiles_dict[cycle].time)))[0])
         pressure = pressure + list(profiles_dict[cycle].pressure)
         pitch = pitch + list(profiles_dict[cycle].pitch)
         roll = roll + list(profiles_dict[cycle].roll)
         heading = heading + list(profiles_dict[cycle].heading)
-    adcp_df = pd.DataFrame({'cast_num': cast_num, 'pressure_ad': pressure, 'pitch_ad': pitch, 'roll_ad': roll, 'heading_ad': heading},index=pd.to_datetime(time))
+    adcp_df = pd.DataFrame(
+        {'cast_num': cast_num, 'pressure_ad': pressure, 'pitch_ad': pitch, 'roll_ad': roll, 'heading_ad': heading},
+        index=pd.to_datetime(time))
     # hotfix as dive 5 appears to be a bench test...
-    bar = adcp_df[adcp_df.cast_num!='0005a']
-    baz = bar[bar.cast_num!='0005b']
+    bar = adcp_df[adcp_df.cast_num != '0005a']
+    baz = bar[bar.cast_num != '0005b']
 
     if combine:
         mission_summary = mission_summary.join(beam_attrs)
@@ -276,4 +295,3 @@ def add_dive_averages(mission_summary, profiles_dict, combine=False):
     else:
         return beam_attrs, baz
 ################################################################################
-
