d66a2a8161cd866dca86c838b443b776b625efea
Callum Rollo <c.rollo@outlook.com>
2020-04-08 18:15:15 +0100

    added reqs to setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     version='0.1.0',
     description='Tools for setting up, running and processing data from a Seaglider fitted with a Nortek 1MHz ',
     author='Callum Rollo',
-    license='BSD-3',
+    license='BSD-3', install_requires=['numpy', 'pandas', 'netCDF4', 'xarray', 'gsw']
 )
